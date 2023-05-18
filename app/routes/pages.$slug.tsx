e7406d65fe2c6ac76db12c1448da657baf11b14f
John Robertson <jwilliamrob@gmail.com>
2023-05-18 15:46:10 -0700

    here goes nothing

---
+++ app/routes/pages.$slug.tsx
@@ -0,0 +1,26 @@
+import type { LoaderArgs } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import page from '../example.json'
+
+export const loader = async ({
+  context,
+  params,
+}: LoaderArgs) => {
+  
+  return await context.TEST.get(params.slug)
+};
+
+export default function Page() {
+  const page = useLoaderData<typeof loader>();
+
+  console.log(page)
+
+  if (!page) throw new Response(null, { status: 404 })
+
+  return (
+    <div>
+      <div><pre>{JSON.stringify(page, null, 2) }</pre></div>
+    </div>
+  );
+}
---
+++ app/routes/pages.tsx
@@ -0,0 +1,36 @@
+import type { LoaderArgs } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData, Outlet, Link } from "@remix-run/react";
+
+export const loader = async ({
+  context,
+  params,
+}: LoaderArgs) => {
+
+  return await context.TEST.list()
+};
+
+export default function pages() {
+  const pages = useLoaderData<typeof loader>();
+
+  console.log(pages)
+
+  if (!pages) throw new Response(null, { status: 404 })
+
+  return (
+    <div>
+      <div>
+        <h1>Pages</h1>
+        <br />
+        {
+          pages.keys.map((page: any) => {
+            return <Link to={`/pages/${page.name}`} key={page.name}>{page.name}</Link>
+          })
+        }
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
